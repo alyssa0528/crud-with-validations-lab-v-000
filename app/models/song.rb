6c29c6bf64a0336ac411bc50299dc756d06642a0
Alyssa Kim <alyssa.kim@gmail.com>
2018-07-04 09:31:37 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,8 @@ class Song < ActiveRecord::Base
   validates :released, inclusion: {in: [true, false]}
   validates :artist_name, presence: true
   validates :release_year, presence: true, if :released?
-  
 
   def released?
     self.released == true
   end
-end 
+end
