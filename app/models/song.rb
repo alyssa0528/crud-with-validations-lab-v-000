9b50609f37e26b55f318127e56e6211a0b82c22e
Alyssa Kim <alyssa.kim@gmail.com>
2018-07-04 09:23:40 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, uniqueness: {scope: :release_year, message: "cannot be repeated by same artist in same year"}
+  
   validates :release_year, presence: true 
 end
