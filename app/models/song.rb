887a2a46b11e0ed08c66ac19ccbd1fb165eeac07
Alyssa Kim <alyssa.kim@gmail.com>
2018-07-04 09:38:54 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,6 +5,7 @@ class Song < ActiveRecord::Base
   validates :release_year,
     presence: true, if: :released?
     numericality: {less_than_or_equal_to: Time.current.year}
+    
   def released?
     self.released == true
   end
