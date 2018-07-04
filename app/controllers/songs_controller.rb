f3058acca7fc159104ded1e2a2fb7115cb8cbf01
Alyssa Kim <alyssa.kim@gmail.com>
2018-07-04 09:43:28 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,7 +8,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(post_params)
+    @song = Song.new(song_params)
     if @song.valid?
       @song.save 
       redirect_to song_path(@song)
