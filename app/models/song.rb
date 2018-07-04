0e36ec683d57e0b598ff4dcfea4b0686485fe16b
Alyssa Kim <alyssa.kim@gmail.com>
2018-07-04 09:32:21 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: {scope: :release_year, message: "cannot be repeated by same artist in same year"}
   validates :released, inclusion: {in: [true, false]}
   validates :artist_name, presence: true
-  validates :release_year, presence: true, if :released?
+  validates :release_year, presence: true, if: :released?
 
   def released?
     self.released == true
